37140faf7276d88e5607c573ee4bb7fd05e37ce5
luuducquang <luuquang203@gmail.com>
2024-06-20 15:45:32 +0700

    update button hover

---
+++ JS/figma5.js
@@ -0,0 +1,16 @@
+var btn_scroll_top = document.querySelector(".scroll-top");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 1000) {
+        btn_scroll_top.style.display = "block";
+    } else {
+        btn_scroll_top.style.display = "none";
+    }
+});
+
+btn_scroll_top.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+    });
+});
